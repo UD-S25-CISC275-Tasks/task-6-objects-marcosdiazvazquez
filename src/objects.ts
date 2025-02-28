94e4ba364dd33bae64dbad48aaa06a94a88a8f56
mdv <marcosdv249@gmail.com>
2025-02-27 23:49:50 -0500

    fixed unnecessary conditional

--- src/objects.ts
+++ src/objects.ts
@@ -43,13 +43,10 @@ export function isCorrect(question: Question, answer: string): boolean {
  * be exactly one of the options.
  */
 export function isValid(question: Question, answer: string): boolean {
-    if (question.type === "multiple_choice_question") {
-        return question.options.includes(answer);
-    } else if (question.type === "short_answer_question") {
-        return true;
-    } else {
-        return false;
-    }
+    return (
+        question.type === "short_answer_question" ||
+        question.options.includes(answer)
+    );
 }
 
 /**
